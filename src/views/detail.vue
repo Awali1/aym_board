b478f61109838a30e5080987dd35ce9123239565
majianqing <majianqing@uspring.cn>
2020-07-21 16:29:51 +0800

    修改样式

--- src/views/detail.vue
+++ src/views/detail.vue
@@ -10,38 +10,45 @@
                         <div class="boxContent" style="position: relative;height: 100%;">
                             <div class="imgBox2">
                                 <img src="../assets/images/mt01.png">
-                                <span>托盘种类</span>
-                                <p>{{task01.tc_code}}</p>
+                                <p>
+                                    <span v-if="task01.task_type == 'PRODPULL'">成品拉动</span>
+                                    <span v-if="task01.task_type == 'MATPULL'">包材拉动</span>
+                                    <span v-if="task01.task_type == 'PALLETPULL'">母托盘拉动</span>
+                                    <span v-if="task01.task_type == 'PRODHOUSE'">成品入立库</span>
+                                    <span v-if="task01.task_type == 'POINTPULL'">点到点拉动</span>
+                                </p>
                             </div>
                             <div class="TitleBox TitleBox2">
                                 <div class="rowBlock">
                                     <p><span class="label">任务号：</span><span>{{task01.task_code}}</span></p>
-                                    <p><span class="label">入库口：</span><span>{{task01.task_path_point}}</span></p>
+                                    <p><span class="label">入库口：</span><span>{{task01.ge_name}}</span></p>
                                 </div>
                                 <div class="rowBlock">
-                                    <p><span class="label">产线：</span><span>{{task01.task_path_point}}</span></p>
-                                    <p><span class="label">优先级：</span><span>{{task01.is_top}}</span></p>
+                                    <p><span class="label">产线：</span><span>{{task01.line_name}}</span></p>
+                                    <p><span class="label">优先级：</span>
+                                       <span :class="task01.is_top? 'status02':'status03'">{{task01.is_top?'二级':'一级'}}</span>
+                                    </p>
                                 </div>
                                 <div class="rowBlock">
-                                    <p><span class="label">物料号：</span><span>{{task01.tc_code}}</span></p>
+                                    <p><span class="label">物料号：</span><span>{{task01.prod_code}}</span></p>
                                     <p><span class="label">产生时间：</span><span>{{task01.create_time}}</span></p>
                                 </div>
                                 <div class="rowBlock">
                                     <p><span class="label">箱号：</span><span>{{task01.box_barcode}}</span></p>
+                                    <p><span class="label">库存地：</span><span>{{task01.erp_warehouse}}</span></p>
                                 </div>
                             </div>
                             <div class="row2 progress2">
                                 <p><span class="label">状态：</span>
-                                    <el-progress :text-inside="true" :stroke-width="18"
-                                        :percentage="parseInt(task01.percent || 0)" status="exception">
-                                    </el-progress>
+                                    <el-progress :text-inside="true" :stroke-width="18" :percentage="parseInt(task01.percent || 0)"
+                                        status="exception"></el-progress>
                                 </p>
-                                <span>接收RCS发出的托盘举起信号</span>
+                                <!-- <span>{{task01.flow_name}}</span> -->
                             </div>
                         </div>
                     </div>
                 </el-col>
-                <el-col class="xyrw">
+                <el-col class="xyrw" v-show="false">
                     <div class="box" style="height: 45vh;">
                         <div class="lineTitle">
                             <p><span>当前任务</span></p>
@@ -75,7 +82,7 @@
                                         :percentage="parseInt(task01.percent || 0)" status="exception">
                                     </el-progress>
                                 </p>
-                                <span>接收RCS发出的托盘举起信号</span>
+                                <!-- <span>接收RCS发出的托盘举起信号</span> -->
                             </div>
                         </div>
                     </div>
@@ -84,35 +91,34 @@
             <el-col :span="16" class="cprk">
                 <div class="box">
                     <div class="title">
-                        成品入库列表
+                        <span style="float: left;">任务流程列表</span>
+                        <el-row style="float: right; margin-right: 2% ">
+                            <el-button type="danger" v-if="nowTaskInfo.task_status != 'CANCEL'" @click="taskCancel">任务取消</el-button>
+                            <el-button type="danger" v-if="nowTaskInfo.task_status == 'CANCEL'" @click="replayTask">重新发送任务</el-button>
+                            <el-button type="danger" @click="taskWarnComplete">任务异常完成</el-button>
+                        </el-row>
                     </div>
-                    <div class="boxContent aymTable">
+                    <div class="boxContent aymTable" style="clear: both">
                         <el-table :data="tableData" :height="residueHeight([80]) + 'px'">
+                            <el-table-column prop="tm_code" align="center" label="流程编号"></el-table-column>
+                            <el-table-column prop="tc_desc" align="center" label="流程描述">
+                                <template slot-scope="scope">
+                                    <span v-if="scope.row.tc_id == nowTaskInfo.now_tc_id">
+                                        <span v-if="nowTaskInfo.is_warning" style="color: red">{{scope.row.tc_desc}}</span>
+                                        <span v-else class="status03">{{scope.row.tc_desc}}</span>
+                                    </span>
+                                    <span v-else>{{scope.row.tc_desc}}</span>
+                                </template>
+                            </el-table-column>
                             <el-table-column prop="task_code" align="center" label="任务号"></el-table-column>
-                            <el-table-column prop="task_path_point" align="center" label="产线"></el-table-column>
-                            <el-table-column prop="" align="center" label="物料号"></el-table-column>
-                            <el-table-column prop="aaa" align="center" label="箱号"></el-table-column>
-                            <el-table-column prop="task_path_point" align="center" label="入库口"></el-table-column>
-                            <el-table-column prop="is_top" align="center" label="优先级">
+                            <el-table-column prop="create_time" align="center" label="创建时间"></el-table-column>
+                            <el-table-column prop="update_time" align="center" label="完成时间">
                                 <template slot-scope="scope">
-                                    <p :class="scope.row.is_top? 'status03':'status02'">
-                                        {{scope.row.is_top? '二级':'一级'}}
-                                    </p>
+                                    <span v-if="scope.row.tc_id == nowTaskInfo.now_tc_id">当前正在执行</span>
+                                    <span v-else>{{scope.row.update_time}}</span>
                                 </template>
                             </el-table-column>
-                            <el-table-column prop="create_time" align="center" label="产生时间"></el-table-column>
-                            <el-table-column align="center" label="状态">
-                                <template slot-scope="scope">
-                                    <p class="status04 borderBox">
-                                        {{scope.row.task_status}}
-                                    </p>
-                                </template>
-                            </el-table-column>
-                            <!-- <el-table-column align="center" label="操作">
-                                <template slot-scope="scope">
-                                    <el-button type="warning" size="small" @click="detail(scope.row)">详情</el-button>
-                                </template>
-                            </el-table-column> -->
+                            <el-table-column prop="tw_content" align="center" label="异常信息"></el-table-column>
                         </el-table>
                     </div>
                 </div>
@@ -126,18 +132,123 @@
         name: 'detail',
         data() {
             return {
-                tableData: [{}],
+                tableData: [],
                 task01: {},
                 task02: {},
 
-                detailData: {}
+                detailData: {},
+
+                nowTaskInfo: '',
             }
         },
         mounted: function () {
             console.log(this.$route.params.detailData)
+            this.task01 = this.$route.params.detailData;
+            this.nowTaskInfo = this.task01;
+
+            this.getTableData();
         },
         methods: {
-            
+            getTableData() {
+                let self = this
+                this.$http.request({
+                    url: '/api/task/flow',
+                    method: 'GET',
+                    params: {
+                        taskCode: this.nowTaskInfo.task_code
+                    }
+                }).then(function (resp) {
+                    console.log(resp);
+                    self.tableData = resp.data;
+                })
+            },
+            taskCancel() {
+                let self = this
+                this.$confirm('是否取消该任务?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.$http.request({
+                        url: '/api/task/cancel',
+                        method: 'POST',
+                        params: {
+                            taskCode: this.nowTaskInfo.task_code
+                        }
+                    }).then(function (resp) {
+                        if(resp.status == 100){
+                            self.$message({
+                                type: 'success',
+                                message: '任务取消成功!'
+                            });
+                        }else{
+                            self.$message({
+                                type: 'warning',
+                                message: resp.message
+                            });
+                        }
+                        self.getTableData();
+                    })
+                })
+            },
+            taskWarnComplete() {
+                let self = this
+                this.$confirm('是否将改任务异常完成?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.$http.request({
+                        url: '/api/task/warnComplete',
+                        method: 'POST',
+                        params: {
+                            taskCode: this.nowTaskInfo.task_code
+                        }
+                    }).then(function (resp) {
+                        if(resp.status == 100){
+                            self.$message({
+                                type: 'success',
+                                message: '异常完成成功!'
+                            });
+                        }else{
+                            self.$message({
+                                type: 'warning',
+                                message: resp.message
+                            });
+                        }
+                        self.getTableData();
+                    })
+                })
+            },
+            replayTask() {
+                let self = this
+                this.$confirm('确定重新发送此任务吗?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.$http.request({
+                        url: '/api/task/replay',
+                        method: 'POST',
+                        params: {
+                            taskCode: this.nowTaskInfo.task_code
+                        }
+                    }).then(function (resp) {
+                        if(resp.status == 100){
+                            self.$message({
+                                type: 'success',
+                                message: '任务重新发送成功!'
+                            });
+                        }else{
+                            self.$message({
+                                type: 'warning',
+                                message: resp.message
+                            });
+                        }
+                        self.getTableData();
+                    })
+                })
+            }
         }
     }
 </script>
--- src/views/product.vue
+++ src/views/product.vue
@@ -2,7 +2,7 @@
     <div class="product">
         <el-row :gutter="10">
             <el-col :span="12" class="dqrw">
-                <div class="box leftBox">
+                <div class="box leftBox" @click="detail(task01)">
                     <div class="lineTitle">
                         <p><span>当前任务</span></p>
                     </div>
@@ -10,7 +10,7 @@
                         <div class="imgBox">
                             <img v-if="task01.is_warning" src="../assets/images/mt01_1.png">
                             <img v-else src="../assets/images/mt01.png">
-                            <span>物料号</span>
+                            <span></span>
                             <p>{{task01.tc_code=='PRODPULLTASK'?'成品入地面链':'成品入立库'}}</p>
                         </div>
                         <div class="TitleBox">
@@ -42,7 +42,7 @@
                 </div>
             </el-col>
             <el-col :span="12" class="xyrw">
-                <div class="box rightBox">
+                <div class="box rightBox" @click="detail(task02)">
                     <div class="lineTitle">
                         <p><span>当前任务</span></p>
                     </div>
@@ -50,8 +50,8 @@
                         <div class="imgBox">
                             <img v-if="task02.is_warning" src="../assets/images/mt01_1.png">
                             <img v-else src="../assets/images/mt01.png">
-                            <span>物料号</span>
-                            <p>{{task02.tc_code}}</p>
+                            <span></span>
+                            <p>{{task01.tc_code=='PRODPULLTASK'?'成品入地面链':'成品入立库'}}</p>
                         </div>
                         <div class="TitleBox">
                            <div class="row">
@@ -72,7 +72,7 @@
                             </div>
                             <div class="row progress">
                                 <p><span class="label">状态：</span>
-                                    <el-progress :text-inside="true" :stroke-width="18" :percentage="parseInt(task02.percent || 0)"
+                                    <el-progress :text-inside="true" :stroke-width="16" :percentage="parseInt(task02.percent || 0)"
                                         status="exception"></el-progress>
                                 </p>
                                 <span>{{task02.flow_name}}</span>
@@ -90,50 +90,83 @@
                         <div  style="float: left;">
                             <span class="selectTitle">单元</span>
                             <el-select v-model="queryParam.unitCode" @change="getLineList();queryData()">
+                                <el-option label="全部" value=""></el-option>
                                 <el-option v-for="(item, index) in unitList" :key="index" :label="item.unitName" :value="item.unitCode">
                                 </el-option>
                             </el-select>
                         </div>
-                        <div>
+                        <div style="float: left;">
                             <span class="selectTitle">产线</span>
                             <el-select v-model="queryParam.lineCode" @change="queryData()">
+                                <el-option label="全部" value=""></el-option>
                                 <el-option v-for="(item, index) in lineList" :key="index" :label="item.lineName" :value="item.lineCode">
                                 </el-option>
+                            </el-select>
+                        </div>
+                        <div>
+                            <span class="selectTitle">任务状态</span>
+                            <el-select v-model="queryParam.taskStatus" @change="queryData()">
+                                <!-- <el-option label="全部" value=""></el-option> -->
+                                <el-option label="任务开始" value="START"></el-option>
+                                <el-option label="任务进行中" value="EXECUTE"></el-option>
+                                <el-option label="任务取消" value="CANCEL"></el-option>
+                                <el-option label="任务正常完成" value="COMPLETE"></el-option>
+                                <el-option label="任务异常完成" value="WARNCOMP"></el-option>
+                                
                             </el-select>
                         </div>
                     </div>
                     
                     <div class="boxContent aymTable">
                         <el-table :data="tableData" :height="residueHeight([300]) + 'px'">
-                            <el-table-column prop="task_code" align="center" label="任务号"></el-table-column>
-                            <el-table-column prop="task_path_point" align="center" label="产线"></el-table-column>
-                            <el-table-column prop="" align="center" label="物料号"></el-table-column>
-                            <el-table-column prop="aaa" align="center" label="箱号"></el-table-column>
-                            <el-table-column prop="task_path_point" align="center" label="入库口"></el-table-column>
+                            <el-table-column prop="task_code" align="center" label="任务号">
+                                <template slot-scope="scope">
+                                    <span v-if="scope.row.is_warning" style="color: red">{{scope.row.task_code}}</span>
+                                    <span v-else>{{scope.row.task_code}}</span>
+                                </template>
+                            </el-table-column>
+                            <el-table-column prop="line_name" align="center" label="产线"></el-table-column>
+                            <el-table-column prop="line_name" align="center" label="完成率">
+                                <template slot-scope="scope">
+                                    <span>{{scope.row.percent}}%</span>
+                                </template>
+                            </el-table-column>
+                            <el-table-column prop="prod_code" align="center" label="物料号"></el-table-column>
+                            <el-table-column prop="box_barcode" align="center" label="箱号"></el-table-column>
+                            <el-table-column prop="erp_warehouse" align="center" label="库存地"></el-table-column>
                             <el-table-column prop="" align="center" label="是否异常">
                                 <template slot-scope="scope">
-                                    <span v-if="scope.row.PAGE_ROW_NUMBER == 1" style="color: red">是</span>
+                                    <span v-if="scope.row.is_warning" style="color: red">是</span>
                                     <span v-else>否</span>
                                 </template>
                             </el-table-column>
                             <el-table-column prop="" align="center" label="异常信息">
                                 <template slot-scope="scope">
-                                    <span style="color:red">{{scope.row.PAGE_ROW_NUMBER == 1?'地面链不允许进入':''}}</span>
+                                    <span style="color:red">{{scope.row.warn_message}}</span>
                                 </template>
                             </el-table-column>
                             <el-table-column align="center" label="优先级">
                                 <template slot-scope="scope">
-                                    <p :class="scope.row.is_top? 'status03':'status02'">
-                                        {{scope.row.is_top? '一级':'二级'}}
+                                    <p :class="scope.row.is_top? 'status02':'status03'">
+                                        {{scope.row.is_top? '二级':'一级'}}
                                     </p>
                                 </template>
                             </el-table-column>
                             <el-table-column prop="create_time" align="center" label="产生时间"></el-table-column>
                             <el-table-column align="center" label="状态">
                                 <template slot-scope="scope">
-                                    <p class="status04 borderBox">
-                                        {{scope.row.task_status}}
+                                    <p class="status04">
+                                        <div class="borderBox" v-if="scope.row.task_status == 'START'">任务开始</div>
+                                        <div class="borderBox" v-if="scope.row.task_status == 'EXECUTE'">任务进行中</div>
+                                        <div class="borderBox" v-if="scope.row.task_status == 'CANCEL'">任务取消</div>
+                                        <div class="borderBox" v-if="scope.row.task_status == 'COMPLETE'">任务正常完成</div>
+                                        <div class="borderBox" v-if="scope.row.task_status == 'WARNCOMP'">任务异常完成</div>
                                     </p>
+                                </template>
+                            </el-table-column>
+                            <el-table-column align="center" label="详情">
+                                <template slot-scope="scope">
+                                    <el-button type="primary" @click="detail(scope.row)">详情</el-button>
                                 </template>
                             </el-table-column>
                         </el-table>
@@ -163,7 +196,7 @@
                     pageNum: 1,
                     pageSize: 10,
                     taskType: 'PRODPULL',
-                    taskStatus: '',
+                    taskStatus: 'EXECUTE',
 
                 }
             }
@@ -175,6 +208,9 @@
         methods: {
             queryData() {
                 let self = this
+
+                self.task01 = {};
+                self.task02 = {};
                 this.$http.request({
                     url: '/api/task',
                     method: 'GET',
@@ -182,8 +218,18 @@
                 }).then(function (resp) {
                     console.log(resp);
                     self.tableData = resp.data.list
-                    self.task01 = self.tableData[0]
-                    self.task02 = self.tableData[1]
+                    if(self.tableData.length == 1){
+                        self.task01 = self.tableData[0]
+                        self.tableData.splice(0, 1);  
+                    }
+
+                    if(self.tableData.length >= 2){
+                        self.task01 = self.tableData[0]
+                        self.task02 = self.tableData[1]
+                        self.tableData.splice(0, 2);  
+                    }
+
+                     
                 })
             },
             getUnitList() {
@@ -207,6 +253,17 @@
                 }).then(function (resp) {
                     console.log(resp);
                     self.lineList = resp.data;
+                })
+            },
+            detail(value){
+                if(value.task_code == undefined){
+                    return;
+                }
+                this.$router.push({
+                    name: 'detail',
+                    params: {
+                        detailData: value
+                    }
                 })
             }
         }
